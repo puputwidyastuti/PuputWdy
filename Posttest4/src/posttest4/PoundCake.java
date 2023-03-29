256df4688e7505e8933a853909da974cf9a2ecf7
puputwidyastuti <91650180+puputwidyastuti@users.noreply.github.com>
2023-03-29 22:01:47 +0800

    Add files via upload

---
+++ Posttest4/src/posttest4/PoundCake.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package posttest4;
+
+/**
+ *
+ * @author acer
+ */
+public class PoundCake extends Bakery {
+//        private String code, rasa;
+//        private int harga, jumlah;
+//           
+        public PoundCake(String code, String rasa, int stok){
+            super(code, rasa, stok);
+        }
+
+    @Override
+    public void getBakeryInformation() {
+        super.getBakeryInformation(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+        
+        System.out.println("Code Cake               : "+ Code);
+        System.out.println("Rasa Cake               : "+ Rasa);
+        System.out.println("Stok yang tersedia      : "+ Stok);
+    }
+       
+        public String getCode() {
+            return Code;
+        }
+
+        public void setCode(String Code) {
+            this.Code = Code;
+        }
+
+        public String getRasa() {
+            return Rasa;
+        }
+
+        public void setRasa(String Rasa) {
+            this.Rasa = Rasa;
+        }
+
+        public int getStok() {
+            return Stok;
+        }
+
+        public void setStok(int Stok) {
+            this.Stok = Stok;
+        }
+        
+        public void getBakeryInformation(boolean showCode){ //overloading
+        if (showCode) getBakeryInformation();
+        else super.getBakeryInformation();
+    }
+
+
+}       
+    //        public int getCost(){
+    //            double hargaPerPoundCake = (double) harga / 12.0;
+    //            int cost = (int) Math.round(hargaPerPoundCake * jumlah);
+    //            return cost;
+    //        }
+
+
