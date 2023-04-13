84881bfd306f8fa36e85470177e5bc42091a0389
puputwidyastuti <91650180+puputwidyastuti@users.noreply.github.com>
2023-04-13 21:02:15 +0800

    Add files via upload

---
+++ POSTTEST6/src/AkunBakery.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author acer
+ */
+public abstract class AkunBakery {
+    protected String username, pass;
+    public AkunBakery(String username, String pass){
+        this.username = username;
+        this.pass = pass;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+}
